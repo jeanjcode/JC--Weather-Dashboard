5a8aa12ae0761acefc15b8d76e6fddb452acee14
JeanJ <jeaniec123@gmail.com>
2023-04-16 16:08:39 +0100

    Added coordinates function & store searches in ls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,77 @@
-// var searchForm = $(".searchForm");
-// var searchBtn = $("#searchBtn");
-// var searchCity = $("#searchCity");
-// var weatherContainer = $("weatherContainer");
-// var currentWeatherContainer = $(".currentWeatherContainer");
+// var openW_Url = "https://api.openweathermap.org/data/2.5/";
+// var apiKey = "de3c367e509f63f28b967e73b8cc9437";
 
-var openW_Url = "https://api.openweathermap.org/data/2.5/";
-var apiKey = "de3c367e509f63f28b967e73b8cc9437";
+var openW_ApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&units=metric&cnt=1&appid=de3c367e509f63f28b967e73b8cc9437`;
+var oneCallApiUrl;
+var defaultCity = "birmingham, gb";
+var weatherData; //
+var coordData;//
+var lat; //
+var lon; //
 
 
 
-//DATE & TIME ABOVE HEADING
+//Date & time
 $("#today").text(moment().format("DD/MM/YYYY HH:mm:ss"));
 
-function updateTime() {
+function currentTime() {
   $("#today").text(moment().format("DD/MM/YYYY HH:mm:ss"));
 }
-setInterval(updateTime, 1000);
+setInterval(currentTime, 1000);
+
+
+
+//Search city coordinates
+function searchCoord() {
+  fetch(openW_ApiUrl)
+    .then(function (response) {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw response;
+      }
+    })
+    .then(function (data) {
+      coordData = data;
+      lat = coordData.city.coord.lat;
+      lon = coordData.city.coord.lon;
+      oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=de3c367e509f63f28b967e73b8cc9437`;
+    })
+    
+    .then(searchWeather)
+    .then(() => $("body"));    
+}
+
+//Store searches
+if (localStorage.getItem("history") == null) {
+  localStorage.setItem("history", "[]");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -24,12 +80,6 @@ setInterval(updateTime, 1000);
 // Cards
 //date
 //temp/icon/wind/humidity
-
-
-
-
-
-
 
 
 
@@ -46,7 +96,7 @@ setInterval(updateTime, 1000);
 
 // 5 day weather forecast:
 // https://openweathermap.org/forecast5
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={de3c367e509f63f28b967e73b8cc9437}
 
 
 
