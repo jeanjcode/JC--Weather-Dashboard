673688165007040d6fce1f6924224d4164f2634e
JeanJ <jeaniec123@gmail.com>
2023-04-16 01:00:58 +0100

    Added css color, html city search + weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,54 @@
-var searchForm = $(".searchForm");
-var searchBtn = $("#searchBtn");
-var searchCity = $("#searchCity");
-var openW_Url = ""
+// var searchForm = $(".searchForm");
+// var searchBtn = $("#searchBtn");
+// var searchCity = $("#searchCity");
+// var weatherContainer = $("weatherContainer");
+// var currentWeatherContainer = $(".currentWeatherContainer");
+
+var openW_Url = "https://api.openweathermap.org/data/2.5/";
 var apiKey = "de3c367e509f63f28b967e73b8cc9437";
 
-var weatherContainer = $("weatherContainer");
-var currentWeatherContainer = $(".currentWeatherContainer");
+
+
+//DATE & TIME ABOVE HEADING
+$("#today").text(moment().format("DD/MM/YYYY HH:mm:ss"));
+
+function updateTime() {
+  $("#today").text(moment().format("DD/MM/YYYY HH:mm:ss"));
+}
+setInterval(updateTime, 1000);
+
+
+
+
+
+// Cards
+//date
+//temp/icon/wind/humidity
+
+
+
+
+
+
+
+
+
+
+
+// https://home.openweathermap.org/users/sign_in
+
+// Current weather data:
+// https://openweathermap.org/current
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+
+
+
+// 5 day weather forecast:
+// https://openweathermap.org/forecast5
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+
+
 
