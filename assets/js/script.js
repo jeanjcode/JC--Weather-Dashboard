5eb6e3aa55896d1082a3ff6a6ab1d0b29795cf18
JeanJ <jeaniec123@gmail.com>
2023-04-17 13:38:57 +0100

    Set up city search function & button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
+var defaultCity = "birmingham, gb";
 var openW_ApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&units=metric&cnt=1&appid=166a433c57516f51dfab1f7edaed8413`;
 var oneCallApiUrl;
-var defaultCity = "birmingham, gb";
 var weatherData; 
 var coordinates;
 var lat; 
@@ -43,6 +43,50 @@ function searchCoord() {
 if (localStorage.getItem("history") == null) {
   localStorage.setItem("history", "[]");
 }
+
+
+
+// function for getting weather at coordinates and call render functions
+function searchWeather() {
+  fetch(oneCallApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      weatherData = data;
+    })
+    .then(renderCurrentWeather)
+    .then(renderForecast)
+   
+}
+searchCoord();
+
+
+
+//User city search 
+function searchforCity(e) {
+  e.preventDefault();
+  if ($("#cityInput").val() === "") {
+    return;
+  }
+  defaultCity = $("#cityInput").val();
+  openW_ApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&units=metric&appid=166a433c57516f51dfab1f7edaed8413`;
+  $("body");
+  searchCoord();
+  history();
+  addSearch();
+  $("#cityInput").val("");
+}
+
+$("#searchButton").click(searchforCity);
+
+
+
+
+
+
+
+
 
 
 
