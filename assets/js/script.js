38b8fae25d528d9e5289732dd51239eaa228b893
JeanJ <jeaniec123@gmail.com>
2023-04-17 13:03:34 +0100

    Tidied up code, weather not rendering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
-// var openW_Url = "https://api.openweathermap.org/data/2.5/";
-// var apiKey = "de3c367e509f63f28b967e73b8cc9437";
-
-var openW_ApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&units=metric&cnt=1&appid=de3c367e509f63f28b967e73b8cc9437`;
+var openW_ApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${defaultCity}&units=metric&cnt=1&appid=166a433c57516f51dfab1f7edaed8413`;
 var oneCallApiUrl;
 var defaultCity = "birmingham, gb";
-var weatherData; //
-var coordData;//
-var lat; //
-var lon; //
+var weatherData; 
+var coordinates;
+var lat; 
+var lon; 
 
 
 
@@ -32,10 +29,10 @@ function searchCoord() {
       }
     })
     .then(function (data) {
-      coordData = data;
-      lat = coordData.city.coord.lat;
-      lon = coordData.city.coord.lon;
-      oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=de3c367e509f63f28b967e73b8cc9437`;
+     coordinates = data;
+      lat =coordinates.city.coord.lat;
+      lon =coordinates.city.coord.lon;
+      oneCallApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly&appid=166a433c57516f51dfab1f7edaed8413`;
     })
     
     .then(searchWeather)
@@ -55,48 +52,19 @@ if (localStorage.getItem("history") == null) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Cards
-//date
-//temp/icon/wind/humidity
-
-
-
+///
 
 
 // https://home.openweathermap.org/users/sign_in
 
 // Current weather data:
 // https://openweathermap.org/current
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={...}
 
 
 // 5 day weather forecast:
 // https://openweathermap.org/forecast5
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={de3c367e509f63f28b967e73b8cc9437}
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={...}
 
 
 
